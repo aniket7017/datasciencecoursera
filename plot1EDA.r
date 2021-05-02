9f238165de39abf3412cae6a349601d181e0f617
aniket7017 <aniketp2604@gmail.com>
2021-05-02 12:32:56 +0530

    commit

---
+++ plot1EDA.r
@@ -0,0 +1,16 @@
+SCC <- readRDS("Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+
+totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
+
+png(filename='plot1EDA.png')
+
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
+
+dev.off()
+
