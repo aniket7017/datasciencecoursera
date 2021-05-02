dcfeeb41ffdc437daad27f05cb98a184d853d53d
aniket7017 <aniketp2604@gmail.com>
2021-05-02 12:35:39 +0530

    commit

---
+++ plot6EDA.R
@@ -0,0 +1,25 @@
+SCC <- readRDS("Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+condition <- grepl("vehicle", SCC[, SCC.Level.Two], ignore.case=TRUE)
+vehiclesSCC <- SCC[condition, SCC]
+vehiclesNEI <- NEI[NEI[, SCC] %in% vehiclesSCC,]
+
+# Subset the vehicles NEI data by each city's fip and add city name.
+vehiclesBaltimoreNEI <- vehiclesNEI[fips == "24510",]
+vehiclesBaltimoreNEI[, city := c("Baltimore City")]
+
+vehiclesLANEI <- vehiclesNEI[fips == "06037",]
+vehiclesLANEI[, city := c("Los Angeles")]
+
+# Combine data.tables into one data.table
+bothNEI <- rbind(vehiclesBaltimoreNEI,vehiclesLANEI)
+
+png("plot6EDA.png")
+
+ggplot(bothNEI, aes(x=factor(year), y=Emissions, fill=city)) +
+  geom_bar(aes(fill=year),stat="identity") +
+  facet_grid(scales="free", space="free", .~city) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Kilo-Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore & LA, 1999-2008"))
+
+dev.off()
